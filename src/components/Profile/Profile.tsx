97960c18afabd8a917525630f35899d2bb0fa532
Alexey <falconaleksey@mail.ru>
2021-10-31 19:12:29 +0300

    dialogs

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -5,7 +5,7 @@ import s from './Profile.module.css'
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgQyG5V9oSD7hP7SZePDYuC56TCQkLWUt6Rg&usqp=CAU'/>
             </div>
